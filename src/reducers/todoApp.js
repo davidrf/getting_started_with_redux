3a99a737adcc53b00e2f0d3566ad49a5dc82e621
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-04-24 16:16:58 -0400

    manually create combineReducers function

--- src/reducers/todoApp.js
+++ src/reducers/todoApp.js
@@ -1,11 +1,15 @@
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
 
-const todoApp = (state = {}, action) => {
-  return {
-    todos: todos(state.todos, action),
-    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce((nextState, key) => {
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {});
   };
 };
+
+const todoApp = combineReducers({ todos, visibilityFilter });
 
 export default todoApp;
