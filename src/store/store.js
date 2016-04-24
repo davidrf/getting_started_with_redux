91bb231be576d7c50a801766cf052fff47c3c251
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-04-24 14:35:55 -0400

    refactor to use redux createStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,5 @@
 import counter from '../reducers/counter';
-
-const createStore = reducer => {
-  let state;
-  let listeners = [];
-
-  const getState = () => state;
-
-  const dispatch = action => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
-
-  const subscribe = listenerToSubscribe => {
-    listeners.push(listenerToSubscribe);
-    return () => {
-      listeners = listeners.filter(listener => listener !== listenerToSubscribe)
-    };
-  };
-
-  dispatch({});
-
-  return { getState, dispatch, subscribe };
-};
+import { createStore } from 'redux';
 
 const store = createStore(counter);
 
